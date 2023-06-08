d3c05b43e35e8152360b9e0a11960728b714a90d
GuminskiAdrian <guminskiadrian34@gmail.com>
2023-06-08 17:22:54 +0200

    begginign of puting fixed data into template

--- script/readJSONdata.js
+++ script/readJSONdata.js
@@ -44,31 +44,67 @@ fetchData().then(() => {
                     <td class='teamForm'>${team.form}</td>
                     `;
 
-        const template = document.getElementById("template");
-        const templateContent = template.content.cloneNode(true);
+        // --------- obsluga danych w template -----------------------------------
+        const template = document
+            .getElementById("template")
+            .content.cloneNode(true);
+
+        const fixture = template.querySelectorAll(".fixture");
+        for (let i = 0; i < 6; i++) {
+            const date = fixture[i].querySelector(".date");
+
+            const homeTeam = fixture[i].querySelector(".home");
+            const homeTeamName = homeTeam.querySelector("p");
+            const homeTeamLogo = homeTeam.querySelector("img");
+
+            const finalOrHour = fixture[i].querySelector(".final");
+
+            const awayTeam = fixture[i].querySelector(".away");
+            const awayTeamName = awayTeam.querySelector("p");
+            const awayTeamLogo = awayTeam.querySelector("img");
+
+            if (i === 0) {
+                // tutaj nic nie ma ponieważ plik JSON nie jest przygotowany na
+                // automatyczne uzupełnienie jeśli skończył się sezon
+            } else {
+                let dateFormated = matches.previousMatch[i-1].fixture.date;
+                dateFormated = new Date(dateFormated);
+
+                const options = {
+                    day: "2-digit",
+                    month: "2-digit",
+                    year: "numeric"
+                };
+
+                dateFormated = dateFormated.toLocaleDateString("en-GB", options).split('/').join('.');
+                date.textContent = `${dateFormated}`
+                
+            }
+        }
+
+        // -----------------------------------------------------------------------
         teamData.setAttribute("class", "show");
         tableBody.appendChild(teamData);
-        tableBody.appendChild(templateContent);
+        tableBody.appendChild(template);
     });
     // ---------------------------------------------------------------------------
 
     // --------- poprzednie mecze ------------------------------------ -----------
-    for (let i = 0; i < 5; i++) {
-        console.log(
-            `${matches.previousMatch[i].teams.home.name} ${matches.previousMatch[i].score.fulltime.home} : ${matches.previousMatch[i].score.fulltime.away} ${matches.previousMatch[i].teams.away.name}`
-        );
-    }
+    // for (let i = 0; i < 5; i++) {
+    //     console.log(
+    //         `${matches.previousMatch[i].teams.home.name} ${matches.previousMatch[i].score.fulltime.home} : ${matches.previousMatch[i].score.fulltime.away} ${matches.previousMatch[i].teams.away.name}`
+    //     );
+    // }
     //---------------------------------------------------------------------------
     //----------------ukrywanie pokazywanie rozwijanej sekcji---------------
     const showHideElement = document.querySelectorAll(".show");
-    showHideElement.forEach( element => {
-        element.addEventListener('click', (event) => {
+    showHideElement.forEach((element) => {
+        element.addEventListener("click", (event) => {
             event.preventDefault();
-            // const expandable = document.querySelector(".expandable");
-            const currentRow = event.target.closest('tr');
+            const currentRow = event.target.closest("tr");
             const expandable = currentRow.nextElementSibling;
-            expandable.classList.toggle("invisible");    
-        })
-    })
+            expandable.classList.toggle("invisible");
+        });
+    });
     //---------------------------------------------------------------------------
-})
+});
